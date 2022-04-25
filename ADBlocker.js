c40693d05d91a8f5ccb95792b8c44a931956109d
123 <yp_20171111@163.com>
2022-04-25 12:42:07 +0800

    测试

--- ADBlocker.js
+++ ADBlocker.js
@@ -2,7 +2,7 @@
 // @name         CustomADBlocker
 // @name:cn      广告拦截,可添加自定义拦截目标
 // @namespace    https://gitee.com/yp_program/
-// @version      0.1.1
+// @version      0.1.2
 // @description  广告拦截,比起一般的预设,可以自定添加指定网址的拦截对象
 // @license MIT
 // @author       youhou999
